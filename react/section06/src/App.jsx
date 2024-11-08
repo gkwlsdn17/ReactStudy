ef06711fea92a14fc0061d7940f1d9459f2403a2
hkkwon123 <hkkwon@dandysoft.kr>
2024-11-08 09:52:45 +0900

    카운터 앱 작성

--- react/section06/src/App.jsx
+++ react/section06/src/App.jsx
@@ -1,11 +1,25 @@
-
+import Viewer from "./components/Viewer"
+import Controller from "./components/Controller"
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  //부모에서 useState를 가져야함
+  const [count, setCount] = useState(0)
+  const onClickButton = (value) => {
+    setCount(count + value)
+  }
 
   return (
-    <>
-      카운터앱
-    </>
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count = {count}/>
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton}/>
+      </section>
+    </div>
   )
 }
 
