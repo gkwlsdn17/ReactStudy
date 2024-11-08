31eff420f93b2e2945ff7cffe32e1d2b551a566a
hkkwon123 <hkkwon@dandysoft.kr>
2024-11-08 11:57:24 +0900

    TodoList 추가 및 렌더링하기

--- react/section08/src/components/List.jsx
+++ react/section08/src/components/List.jsx
@@ -1,15 +1,43 @@
+import { useEffect } from 'react'
 import './List.css'
 import TodoItem from './TodoItem'
+import { useState } from 'react'
 
-const List = () => {
+const List = ({todos}) => {
+    const [search, setSearch] = useState("")
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const getFilteredData = () => {
+        if(search === ""){
+            return todos;
+        }
+
+        let data = todos.filter((todo)=>{
+            return todo.content
+            .toLowerCase()
+            .includes(search.toLowerCase())
+        })
+        return data
+    }
+
+    // 리랜더링 될때마다 실행
+    const filteredTodos = getFilteredData()
+    
     return (
         <div className="List">
             <h4>Todo List </h4>
-            <input placeholder="검색어를 입력하세요"/>
+            <input 
+                placeholder="검색어를 입력하세요"
+                value={search}
+                onChange={onChangeSearch}
+            />
             <div className='Todos_wrapper'>
-                <TodoItem/>
-                <TodoItem/>
-                <TodoItem/>
+                {filteredTodos.map((todo)=>{
+                    return <TodoItem key={todo.id} {...todo}/>
+                })}
             </div>
         </div>
     )
